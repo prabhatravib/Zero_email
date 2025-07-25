6378b39198ab94e3937d413d4365b0792e48466a
prabhatravib <prabhatravib@gmail.com>
2025-07-25 00:10:27 -0400

    changes

--- apps/mail/providers/client-providers.tsx
+++ apps/mail/providers/client-providers.tsx
@@ -1,37 +1,9 @@
-import { useKeyboardLayout } from '@/components/keyboard-layout-indicator';
-import { LoadingProvider } from '@/components/context/loading-context';
-import { NuqsAdapter } from 'nuqs/adapters/react-router/v7';
-import { SidebarProvider } from '@/components/ui/sidebar';
-import { PostHogProvider } from '@/lib/posthog-provider';
-import { Provider as JotaiProvider } from 'jotai';
 import type { PropsWithChildren } from 'react';
-import Toaster from '@/components/ui/toast';
-import { ThemeProvider } from 'next-themes';
 
 export function ClientProviders({ children }: PropsWithChildren) {
-  useKeyboardLayout();
-
-  const theme = 'system';
-
   return (
-    <NuqsAdapter>
-      <JotaiProvider>
-        <ThemeProvider
-          attribute="class"
-          enableSystem
-          disableTransitionOnChange
-          defaultTheme={theme}
-        >
-          <SidebarProvider>
-            <PostHogProvider>
-              <LoadingProvider>
-                {children}
-                <Toaster />
-              </LoadingProvider>
-            </PostHogProvider>
-          </SidebarProvider>
-        </ThemeProvider>
-      </JotaiProvider>
-    </NuqsAdapter>
+    <div>
+      {children}
+    </div>
   );
 }
