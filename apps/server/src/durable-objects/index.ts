13ec701bf3b87bea775046826b5b01dc1c553955
prabhatravib <prabhatravib@gmail.com>
2025-07-26 22:55:06 -0400

    changes

--- apps/server/src/durable-objects/index.ts
+++ apps/server/src/durable-objects/index.ts
@@ -1,3 +1,4 @@
 export { ZeroAgent } from './ZeroAgent';
 export { ZeroDB } from './ZeroDB';
-export { ZeroDriver } from './ZeroDriver'; 
+export { ZeroDriver } from './ZeroDriver';
+export { ZeroMCP } from './ZeroMCP'; 
